e508ff906bc975a08cd41238a40c1c2b509b40a2
M-Ali-Haider <mah01134212100@gmail.com>
2023-10-06 00:03:21 +0500

    added the OurGoal descriptions

--- src/Components/OurGoal.tsx
+++ src/Components/OurGoal.tsx
@@ -17,24 +17,21 @@ const OurGoal = () => {
             To Upscale your Business to the Next Level
           </h1>
           <p className="ourgoaltextp">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia itaque
-            officiis, illo in consequuntur laboriosam dicta molestias?
-            Voluptates magni magnam natus voluptatum, eos neque, ea optio non
-            atque ipsam laborum?
+            We are dedicated to elevating your business to new heights. Our goal is to provide you with innovative solutions and strategic insights that empower your brand, expand your reach, and maximize your potential for success.
           </p>
           <div className="OurGoalicons">
             <div className="OurGoaliconscard">
               <div className="goalicon"></div>
               <div className="goalicontext">
                 <h3>Authenticity</h3>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+                <p>We value the genuine essence of your brand. Our focus is on creating authentic experiences for your audience. From design elements to communication strategies, we ensure that your brand's unique identity shines through, fostering genuine connections with your customers.</p>
               </div>
             </div>
             <div className="OurGoaliconscard">
               <div className="goalicon"></div>
               <div className="goalicontext">
                 <h3>Pro Support</h3>
-                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+                <p>Count on us for professional and dedicated support throughout your journey. Our team of experts is here to assist you at every step, providing proactive solutions, expert guidance, and timely assistance. Your success is our priority, and our proactive support ensures you're always in capable hands.</p>
               </div>
             </div>
           </div>
