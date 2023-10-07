bd34c9facb9945b0c3c0d23d800bf3c00e947e1e
M-Ali-Haider <mah01134212100@gmail.com>
2023-10-07 11:54:40 +0500

    bruh

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Footer from "./Components/Footer";
 import GetStartedNow from "./Components/GetStartedNow";
 import WhyChooseUs from "./Components/WhyChooseUs";
 import ScrollToTop from "./Components/ScrollToTop";
-import ThreeDImage from "./STLViewer";
 
 
 const App = () => {
@@ -29,7 +28,7 @@ const App = () => {
         <Header />
         <div className="mainPagewelc">
           <MainPageWel />
-          <ThreeDImage />
+          
           <img className="htmlphp" src={htmlphp} alt="" />
         </div>
       </div>
